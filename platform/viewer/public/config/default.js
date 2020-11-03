732f1260c5d57ac760d7f2b3a69cee131841d20f
Matthias Goetzke <m.goetzke@curasystems.de>
2020-11-03 16:40:52 +0100

    reset config

--- platform/viewer/public/config/default.js
+++ platform/viewer/public/config/default.js
@@ -9,9 +9,9 @@ window.config = {
     dicomWeb: [
       {
         name: 'DCM4CHEE',
-        wadoUriRoot: 'http://localhost:9587/dicomweb/wado',
-        qidoRoot: 'http://localhost:9587/dicomweb/rs',
-        wadoRoot: 'http://localhost:9587/dicomweb/rs',
+        wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
+        qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+        wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
         qidoSupportsIncludeField: true,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
